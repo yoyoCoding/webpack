bf88c27adaee3226abe1d99f0036631be836c398
yyy <yaoyingying@keywa.com>
2019-07-11 09:26:50 +0800

    init
    初始化

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import css from './css/index.css';
-import less from './css/black.less';
-import sass from './css/black.scss';
-
-console.log('hello!');
-// console.log(document.getElementById('mydiv'));
-document.getElementById('mydiv').innerHTML="hello webpack!";
